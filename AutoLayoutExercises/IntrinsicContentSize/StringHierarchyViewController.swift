1627b675e280b8f42ae815b177f9db679c3e3c1b
Dominik Olędzki <dominik.oledzki@gmail.com>
2017-12-10 23:48:33 +0000

    Directories instead of views

--- AutoLayoutExercises/IntrinsicContentSize/StringHierarchyViewController.swift
+++ AutoLayoutExercises/IntrinsicContentSize/StringHierarchyViewController.swift
@@ -14,36 +14,21 @@ class StringHierarchyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        hierarchyView.model =
-//            .init(value: "~", children: [
-//                .init(value: "Applications", children: [
-//                    .init(value: "Xcode", children: []),
-//                    .init(value: "Sketch", children: []),
-//                    .init(value: "Starcraft II", children: [])
-//                    ]),
-//                .init(value: "Desktop", children: [
-//                    .init(value: "Screen Shot 2017-11-25 at 02.17.42", children: [])
-//                    ]),
-//                .init(value: "Documents", children: [
-//                    .init(value: "Projects", children: [
-//                        .init(value: "AutoLayoutExercises", children:  [])
-//                        ])
-//                    ])
-//                ])
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        if let window = self.view.window {
-            hierarchyView.model = Hierarchy.from(view: window)
-        }
-    }
-}
-
-extension Hierarchy where Element == String {
-    static func from(view: UIView) -> Hierarchy {
-        return Hierarchy(value: view.description, children: view.subviews.map({ (view) -> Hierarchy in
-            return Hierarchy.from(view: view)
-        }))
+        hierarchyView.model =
+            .init(value: "~", children: [
+                .init(value: "Applications", children: [
+                    .init(value: "Xcode", children: []),
+                    .init(value: "Sketch", children: []),
+                    .init(value: "Starcraft II", children: [])
+                    ]),
+                .init(value: "Desktop", children: [
+                    .init(value: "Screen Shot 2017-11-25 at 02.17.42", children: [])
+                    ]),
+                .init(value: "Documents", children: [
+                    .init(value: "Projects", children: [
+                        .init(value: "AutoLayoutExercises", children:  [])
+                        ])
+                    ])
+                ])
     }
 }
